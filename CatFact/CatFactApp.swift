17a6852a0fc102744a9a28614dd3553669a37173
Volodymyr Volynets <volodymyr.volynets@student.griffith.ie>
2022-09-26 11:40:02 +0100

    Initial Commit

---
+++ CatFact/CatFactApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatFactApp.swift
+//  CatFact
+//
+//  Created by Vova on 26/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct CatFactApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
